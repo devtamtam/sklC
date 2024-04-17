e32dbdd5c84c810e3be027b29fcee67304157022
devtamtam <115604773+devtamtam@users.noreply.github.com>
2024-04-17 14:26:28 +0900

    Update kadai02-01.cpp

---
+++ NumericalAnalysis/hw02/kadai02-01.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double a=70.0/100.0;
+    double sum=1.0;
+    double b=pow(a,2.0);
+    int nmax=10;
+
+    for(int n=0;n<1+nmax;n++){
+        sum=sum+pow(a)
+    }
+
+
+    printf("a is %f",a);
+    printf("b is %f",b);
+
+
+    return 0;
+}
