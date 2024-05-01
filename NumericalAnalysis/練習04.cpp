db5697359a38aaff5b9f0bc34943eade5f42a02c
devtamtam <115604773+devtamtam@users.noreply.github.com>
2024-05-01 13:26:15 +0900

    Add files via upload

---
+++ NumericalAnalysis/練習04.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+	int n=3;
+    double a[3][3] ={
+		{2.0, 1.0, -3.0},
+		{2.0, -1.0, -1.0},
+		{1.0, -1.0, -2.0}
+	};
+	double y[3] = {-2.0, -2.0, -5.0};
+	double z;
+	int i;
+	printf("  \n");
+    for(i=0;i<n;i++){
+		printf(" a[%d,1]=%e\t a[%d,2]=%e\t a[%d,3]=%e\t y[%d]=%e\n",
+			i,a[i][0],i,a[i][1],i,a[i][2],i,y[i]);
+	}
+	printf("  \n");
+
+	for(i=0;i<n;i++){
+		z=a[i][i];
+		for(int j=i;j<n;j++){
+			a[i][j]=a[i][j]/z;
+		}
+		y[i]=y[i]/z;
+		for(int k=i+1;k<n;k++){
+			z=a[k][i];
+			for(int j=i;j<n;j++){
+				a[k][j]=a[k][j]-a[i][j]*z;
+			}
+			y[k]=y[k]-y[i]*z;
+		}
+	}
+
+	for(i=n-1;i>0;i=i-1){
+		for(int k=i-1;k>=0;k=k-1){
+			z=a[k][i];
+			for(int j=k+1;j<n;j++){
+				a[k][j]=a[k][j]-a[i][j]*z;
+			}
+			y[k]=y[k]-y[i]*z;
+		}
+	}
+    for(i=0;i<n;i++){
+		printf(" y[%d]=%e\n",i,y[i]);
+	}
+	return 0;
+}
